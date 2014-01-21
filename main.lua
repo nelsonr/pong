58be03ac3a1934dd8df160d3ce1de0d1607045dc
Nelson Rodrigues <nelson@fullfront.pt>
2014-01-21 21:55:23 +0000

    Added gamestate

--- main.lua
+++ main.lua
@@ -17,28 +17,53 @@ function love.draw()
 
 	-- draw ball
 	ball.draw()
+
+	-- draw gameover message
+	if gamestate == 'gameover' then
+		if playerA.score == 10 then score.gameover(playerA) end
+		if playerB.score == 10 then score.gameover(playerB) end
+	end
 end
 
 function love.update(dt)
-	-- update pads
-	pad.move(padLeft, dt)
-	pad.move(padRight, dt)
+	if gamestate == 'playing' then
+		-- update pads
+		pad.move(padLeft, dt)
+		pad.move(padRight, dt)
 
-	-- update ball
-	collision(collision(ball.move(dt), padLeft), padRight)
+		-- update ball
+		ball.move(dt)
 
-	-- watch score
-	score.watch(ball)
+		-- test for collisions
+		collision(ball, padLeft)
+		collision(ball, padRight)
+
+		-- watch score
+		score.watch(ball)
+	end
+
+	if gamestate == 'gameover' then
+		-- start new game
+		if love.keyboard.isDown('return') then
+			love.load()
+		end
+	end
+	
+	if playerA.score == 10 or playerB.score == 10  then
+		gamestate = 'gameover'
+	end
 end
 
 function love.load()
 	love.window.setTitle('Super Master Pong')
 	love.window.setMode(640, 480)
-
+	
 	love.graphics.setFont(love.graphics.newFont(30))
 
 	field.load()
 	pad.load()
 	ball.load()
 	score.load()
+
+	gamestate = 'playing'
 end
--- score.lua
+++ score.lua
@@ -5,7 +5,7 @@ function score.draw()
 	local centerX = love.window.getWidth() / 2
 
 	love.graphics.setColor(30, 30, 30)
-	love.graphics.print(playerA.score, centerX - 45, 15)
+	love.graphics.printf(playerA.score, centerX - 60, 15, 45, 'center')
 	love.graphics.print(playerB.score, centerX + 30, 15)
 end
 
@@ -13,11 +13,13 @@ function score.watch(ball)
 	if ball.x < 0 then 
 		playerB.score = playerB.score + 1 
 		ball.reset()
+		ball.speed = ball.speed + 10
 	end
 
 	if ball.x > love.window.getWidth() then 
 		playerA.score = playerA.score + 1 
 		ball.reset()
+		ball.speed = ball.speed + 10
 	end
 end
 
@@ -27,4 +29,13 @@ function score.load()
 	
 	playerB.name = 'Red'
 	playerB.score = 0
+end
+
+function score.gameover(player)
+	local centerX = love.graphics.getWidth() / 2
+	local centerY = love.graphics.getHeight() / 2
+	local message = player.name .. ' Wins!'
+
+	love.graphics.setColor(30, 30, 30)
+	love.graphics.printf(message, centerX - 100, centerY - 25, 200, 'center')
 end
