b411159700628886f63d979a28b9d960b24ebbc9
Nelson Rodrigues <nelson@fullfront.pt>
2014-01-22 14:03:44 +0000

    Abstracted pad movement

--- pad.lua
+++ pad.lua
@@ -1,6 +1,21 @@
 pad = {}
 padLeft, padRight = {}, {}
 
+local function move_up(pad, dt)
+	pad.y = pad.y - (pad.speed * dt)
+	
+	if pad.y < 0 then pad.y = 0 end
+end
+
+local function move_down(pad, dt)
+	pad.y = pad.y + (pad.speed * dt)
+
+	-- set bottom boundary
+	if (pad.y + pad.height) > love.window.getHeight() then
+		pad.y = love.window.getHeight() - pad.height		
+	end
+end
+
 function pad.draw(pad)
 	-- pad zone
 	love.graphics.setColor(240, 240, 240)
@@ -12,20 +27,8 @@ function pad.draw(pad)
 end
 
 function pad.move(pad, dt)
-	if love.keyboard.isDown(pad.upKey) then
-		pad.y = pad.y - (pad.speed * dt)
-
-		if pad.y < 0 then pad.y = 0 end -- set top boundary
-	end
-
-	if love.keyboard.isDown(pad.downKey) then
-		pad.y = pad.y + (pad.speed * dt)
-
-		-- set bottom boundary
-		if (pad.y + pad.height) > love.window.getHeight() then
-			pad.y = love.window.getHeight() - pad.height		
-		end
-	end	
+	if love.keyboard.isDown(pad.upKey) then move_up(pad, dt) end
+	if love.keyboard.isDown(pad.downKey) then move_down(pad, dt) end	
 
 	return pad
 end
@@ -33,18 +36,11 @@ end
 function pad.ai(pad, ball, dt)
 	if ball.x < love.window.getWidth() / 2 and ball.xDirection == -1 then
 		if ball.y < pad.y then 
-			pad.y = pad.y - (pad.speed * dt)
-
-			if pad.y < 0 then pad.y = 0 end
+			move_up(pad, dt)		
 		end	
 
 		if ball.y + ball.height > pad.y + pad.height then
-			pad.y = pad.y + (pad.speed * dt)
-
-			-- set bottom boundary
-			if (pad.y + pad.height) > love.window.getHeight() then
-				pad.y = love.window.getHeight() - pad.height		
-			end
+			move_down(pad, dt)
 		end
 	end
 end
